1f3a0ce1bc125581bca29afd39c5d1466918c434
Markus Flambard <markus.flambard@youcal.com>
2023-12-26 23:14:59 +0100

    No point in dividing simple and compound entity tests

--- test/gtp_entity_tests.erl
+++ test/gtp_entity_tests.erl
@@ -1,4 +1,4 @@
--module(gtp_simple_entity_tests).
+-module(gtp_entity_tests).
 -include_lib("eunit/include/eunit.hrl").
 
 encode_int_test() ->
@@ -13,3 +13,10 @@ encode_vertex_test() ->
 
 decode_vertex_test() ->
     {{j, 11}, []} = gtp_types:decode_vertex(<<"j11">>).
+
+encode_list_of_ints_test() ->
+    EncodedList = gtp_types:encode_list(fun gtp_types:encode_int/1, [9, 8, 7, 6, 5, 4, 3]),
+    <<"9 8 7 6 5 4 3">> = iolist_to_binary(EncodedList).
+
+decode_list_of_ints_test() ->
+    [9, 8, 7, 6, 5, 4, 3] = gtp_types:decode_list(fun gtp_types:decode_int/1, <<"9 8 7 6 5 4 3">>).
