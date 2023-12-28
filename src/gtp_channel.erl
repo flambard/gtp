4ac43e5b1f802ad9268b990eba4ccd1d0953b898
Markus Flambard <markus.flambard@youcal.com>
2023-12-28 22:35:22 +0100

    Fix type specs on gtp_channel

--- src/gtp_channel.erl
+++ src/gtp_channel.erl
@@ -4,7 +4,7 @@
 
 -callback controlling_process(channel(), pid()) -> ok | {error, Reason :: term()}.
 
--callback send_message(channel(), Message :: iolist()) ->
+-callback send_message(channel(), Message :: iodata()) ->
     ok | {error, Reason :: term()}.
 
 -callback stop(channel()) -> ok.
@@ -16,7 +16,7 @@
 %%% API
 %%%
 
--spec recv_message(ControllingProcess :: pid(), Message :: iolist()) -> ok.
+-spec recv_message(ControllingProcess :: pid(), Message :: binary()) -> ok.
 
 recv_message(ControllingProcess, Message) ->
     ControllingProcess ! {gtp, Message},
